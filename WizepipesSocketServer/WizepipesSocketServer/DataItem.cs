8be256265576916931b4d027742a69bcde777823
wei li <liweiliwei0103@outlook.com>
2017-07-14 17:33:15 +0800

    经过使用后，数据缓存的queue是占用内存的关键，处理完之后好多了。（1000模拟50ms，1009字节）

---
+++ WizepipesSocketServer/WizepipesSocketServer/DataItem.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+
+namespace WizepipesSocketServer
+{
+    class DataItem
+    {
+        public Socket socket;
+        public byte[] buffer;
+        public string strAddress;
+        public Queue<byte[]> dataQueue = new Queue<byte[]>();
+
+        private delegate void AsyncAnalyzeData(byte[] data);
+
+        public void OnReceive(IAsyncResult ar)
+        {
+            string message = "";
+            try
+            {
+                int bytesRead = socket.EndReceive(ar);//接收到的数据长度
+                if (bytesRead == 0)
+                {
+                    CloseSocket(socket);//关闭socket
+                }
+                else if (buffer[0] == 0xA5 && buffer[1] == 0xA5 && buffer[bytesRead - 2] == 0x5A && buffer[bytesRead - 1] == 0x5A)//判断报文头和尾
+                {
+                    byte[] recData = new byte[bytesRead];
+                    Array.Copy(buffer, recData, bytesRead);
+
+                    //Console.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff") + "从地址为"+strAddress+"--收到数据" + byteToHexStr(recData)+"\r\n");
+
+                    dataQueue.Enqueue(recData);//Enqueue 将对象添加到 Queue<T> 的结尾处
+
+                }
+                socket.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, OnReceive, this);
+            }
+            catch (Exception ex)
+            {
+                string error = DateTime.Now.ToString() + "出错信息：" + "---" + ex.Message + "\n";
+                System.Diagnostics.Debug.WriteLine(error);
+            }
+        }
+
+        public void HandleData()
+        {
+            //Console.WriteLine(DateTime.Now.ToString()+"\r\n");
+            if (dataQueue.Count > 0)
+            {
+                byte[] datagramBytes = dataQueue.Dequeue();// Dequeue 移除并返回位于 Queue<T> 开始处的对象
+                AsyncAnalyzeData method = new AsyncAnalyzeData(AnalyzeData);
+                method.BeginInvoke(datagramBytes, null, null);
+            }
+        }
+
+        public void AnalyzeData(byte[] datagramBytes)
+        {
+            switch (datagramBytes[2])
+            {
+                case 0x23:
+                    Console.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff") + "收到AD采样数据\r\n");
+                    break;
+                default:
+                    Console.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff") + "收到其他类型数据\r\n");
+                    break;
+            }
+        }
+
+        public void CloseSocket(Socket socket)
+        {
+            try
+            {
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+            }
+            catch (Exception ex)
+            {
+                string error = DateTime.Now.ToString() + "出错信息：" + "---" + ex.Message + "\n";
+                //Log.Debug(error);
+                System.Diagnostics.Debug.WriteLine(error);
+            }
+        }
+
+        // 字节数组转16进制字符串 
+        public static string byteToHexStr(byte[] bytes)
+        {
+            string returnStr = "";
+            if (bytes != null)
+            {
+                for (long i = 0; i < bytes.Length; i++)
+                {
+                    returnStr += bytes[i].ToString("X2");
+                }
+            }
+            return returnStr;
+        }
+
+    }
+}
--- WizepipesSocketServer/WizepipesSocketServer/SocketServer.cs
+++ WizepipesSocketServer/WizepipesSocketServer/SocketServer.cs
@@ -15,35 +15,41 @@ namespace WizepipesSocketServer
     class SocketServer
     {    
         public static Hashtable htAddressItem = new Hashtable();
-        public static Hashtable htIDItem = new Hashtable();
         public static Socket ServerSocket;
         
         public int perPackageLength = 1009;//每包的长度
         public int checkDataQueueTimeInterval = 100;// 检查数据包队列时间休息间隔(ms)
         public bool IsServerOpen = true;
+        private ManualResetEvent checkDataQueueResetEvent = new ManualResetEvent(true);//ManualResetEvent:通知一个或多个正在等待的线程已发生事件
 
-        private ManualResetEvent checkDataQueueResetEvent;//ManualResetEvent:通知一个或多个正在等待的线程已发生事件
-
-        public void OpenServer(string ip, int port)
+        public bool OpenServer(string ip, int port)
         {
             try
-            {              
-                ServerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            {
+                if (IsServerOpen == true)
+                {
+                    ServerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                    IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Parse(ip), port);
 
-                IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+                    //Bind and listen on the given address
+                    ServerSocket.Bind(ipEndPoint);
+                    ServerSocket.Listen(100);
 
-                //Bind and listen on the given address
-                ServerSocket.Bind(ipEndPoint);
-                ServerSocket.Listen(100);
+                    ServerSocket.BeginAccept(new AsyncCallback(OnAccept), null);
 
-                ServerSocket.BeginAccept(new AsyncCallback(OnAccept), null);
+                    //数据包处理线程
+                    if (!ThreadPool.QueueUserWorkItem(this.CheckDatagramQueue))
+                        return false;
 
-                //if (!ThreadPool.QueueUserWorkItem(this.CheckDatagramQueue)) return false;//数据包处理线程
+                    return true;
+                }
+                else return false;
             }
             catch (Exception ex)
             {
                 string error = DateTime.Now.ToString() + "出错信息：" + "---" + ex.Message + "\n";
                 System.Diagnostics.Debug.WriteLine(error);
+                return false;
             }
         }
 
@@ -58,16 +64,15 @@ namespace WizepipesSocketServer
                 string strAddress = clientSocket.RemoteEndPoint.ToString();
                 if (!htAddressItem.ContainsKey(strAddress))
                 {
-                    AddressItem addressItem = new AddressItem();
-                    addressItem.socket = clientSocket;
+                    DataItem dataItem = new DataItem();
+                    dataItem.socket = clientSocket;
 
-                    addressItem.buffer = new byte[perPackageLength];
-                    addressItem.strAddress = strAddress;
-                    htAddressItem.Add(strAddress, addressItem);
+                    dataItem.buffer = new byte[perPackageLength];
+                    dataItem.strAddress = strAddress;
+                    htAddressItem.Add(strAddress, dataItem);
                     //Once the client connects then start receiving the commands from her
                     //开始从连接的socket异步接收数据
-                    addressItem.socket.BeginReceive(addressItem.buffer, 0, addressItem.buffer.Length, SocketFlags.None,
-                    new AsyncCallback(addressItem.OnReceive), addressItem);
+                    dataItem.socket.BeginReceive(dataItem.buffer, 0, dataItem.buffer.Length, SocketFlags.None, dataItem.OnReceive, dataItem);
                 }
             }
             catch (Exception ex)
@@ -81,22 +86,22 @@ namespace WizepipesSocketServer
         private void CheckDatagramQueue(object state)
         {
             checkDataQueueResetEvent.Reset(); //Reset()将事件状态设置为非终止状态，导致线程阻止。
-
             while (IsServerOpen)
             {
-                           
-                Thread.Sleep(checkDataQueueTimeInterval);
+                try
+                {
+                    foreach (DataItem dataItem in htAddressItem.Values)
+                    {
+                        dataItem.HandleData();
+                    }
+                }
+                catch (Exception ex)
+                { System.Diagnostics.Debug.WriteLine(ex); }
+                Thread.Sleep(checkDataQueueTimeInterval);//当前数据处理线程休眠一段时间
             }
-
+            
             checkDataQueueResetEvent.Set();
         }
-
-        private void CloseSocket()
-        {
-
-        }
-
-
 
     }
 }
